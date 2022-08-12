bb9601f36f732a26caa0234651edd7f5a246d8a5
Rob Sameh <rob.sameh@gmail.com>
2022-08-12 16:47:43 -0700

    added pictures to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
+import diceTwenty from './dice-twenty.svg';
+import meepleArmy from './meeple-army.svg';
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>Board Game Widget Store</h1>
+      <h1>
+      <img src={diceTwenty} alt="d20" width="100" height="125"/> Board Game Widget Store<img src={meepleArmy} alt="meeples" width="100" height="125"/>
+      </h1>
     </React.Fragment>
-  );
+  )
 }
 
 export default Header;
